518c130b0b334bd093251b75c9dec6be84eb0571
Victor Chen <yche276@gmail.com>
2015-04-14 23:06:08 +1200

    update

--- include/DigitalMt.h
+++ include/DigitalMt.h
@@ -21,3 +21,9 @@
 #define TT_RELEASE_SAFELY(__POINTER) { [__POINTER release]; __POINTER = nil; }
 
 
+#define IS_IPAD   (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+#define IS_IPHONE_5 (IS_IPHONE && [[UIScreen mainScreen] bounds].size.height == 568.0f)
+#define IS_IPHONE_4 (IS_IPHONE && [[UIScreen mainScreen] bounds].size.height == 480.0f)
+
+
