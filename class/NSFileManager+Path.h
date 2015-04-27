6f3883ca526be700b97f739118ef602737b338b6
Victor Chen <yche276@gmail.com>
2015-04-27 18:49:29 +1200

    update path category

--- class/NSFileManager+Path.h
+++ class/NSFileManager+Path.h
@@ -15,4 +15,28 @@
 + (NSURL *)documentsDirectoryURL;
 + (NSString *)documentsDirectoryPath;
 
+
++ (NSURL *)libraryDirectoryURL;
++ (NSString *)libraryDirectoryPath;
+
+
++ (NSURL *)temporaryDirectoryURL;
++ (NSString *)temporaryDirectoryPath;
+
++ (double)availableDiskSpace;
+
+/**
+ Retrieve the path of files with specific extensions
+ Sample usage:
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSArray *extensions = [NSArray arrayWithObjects:@"txt", @"png" nil];
+ 
+    //returns all txt and png files in the document directory
+    [NSFileManager filesOfDirectoryAtPath:documentsDirectory extensions:extensions];
+ 
+ 
+ */
++ (NSArray *)filesOfDirectoryAtPath:(NSString *)strPath extensions:(NSArray *)aryExtensions;
+
 @end
--- class/NSFileManager+Path.m
+++ class/NSFileManager+Path.m
@@ -13,9 +13,43 @@
 + (NSURL *)documentsDirectoryURL{
     return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
 }
-
 + (NSString *)documentsDirectoryPath{
-    return @"";
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentPath = [paths objectAtIndex:0];
+    return documentPath;
+
+    return NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
+}
+
++ (NSURL *)libraryDirectoryURL{
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSLibraryDirectory inDomains:NSUserDomainMask] lastObject];
+}
++ (NSString *)libraryDirectoryPath{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+    NSString *libraryPath = [paths objectAtIndex:0];
+    return libraryPath;
+}
+
++ (NSURL *)temporaryDirectoryURL{
+    return [NSURL URLWithString:NSTemporaryDirectory()];
+}
++ (NSString *)temporaryDirectoryPath{
+    return NSTemporaryDirectory();
+}
+
++ (double)availableDiskSpace
+{
+    NSDictionary *attributes = [[NSFileManager defaultManager] attributesOfFileSystemForPath:self.documentsDirectoryPath
+                                                                                       error:nil];
+    return [attributes[NSFileSystemFreeSize] unsignedLongLongValue] / (double)0x100000;
+}
+
++ (NSArray *)filesOfDirectoryAtPath:(NSString *)strPath extensions:(NSArray *)aryExtensions{
+    
+    NSArray *dirContents = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:strPath
+                                                                               error:nil];
+    return [dirContents filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"pathExtension IN %@", aryExtensions]];
+    
 }
 
 @end
